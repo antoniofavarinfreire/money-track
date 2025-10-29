212ead29850394f6388431a0f4576116a9a5d48a
Antonio Favarin Freire <42979335+antoniofavarinfreire@users.noreply.github.com>
2025-10-28 21:39:27 -0300

    feat: ajustes no dash

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content">
     <div class="md-layout">
-      <div
+      <!-- <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
       >
         <chart-card
@@ -72,7 +72,7 @@
             </div>
           </template>
         </chart-card>
-      </div>
+      </div> -->
       <div
         class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
       >
@@ -82,15 +82,8 @@
           </template>
 
           <template slot="content">
-            <p class="category">Revenue</p>
-            <h3 class="title">$34,245</h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>date_range</md-icon>
-              Last 24 Hours
-            </div>
+            <p class="category">Cartoes credito</p>
+            <h3 class="title">R$34,245</h3>
           </template>
         </stats-card>
       </div>
@@ -103,18 +96,8 @@
           </template>
 
           <template slot="content">
-            <p class="category">Used Space</p>
-            <h3 class="title">
-              49/50
-              <small>GB</small>
-            </h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon class="text-danger">warning</md-icon>
-              <a href="#pablo">Get More Space...</a>
-            </div>
+            <p class="category">Vale refeição</p>
+            <h3 class="title">R$34,245</h3>
           </template>
         </stats-card>
       </div>
@@ -127,15 +110,8 @@
           </template>
 
           <template slot="content">
-            <p class="category">Fixed Issues</p>
-            <h3 class="title">75</h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>local_offer</md-icon>
-              Tracked from Github
-            </div>
+            <p class="category">Gastos Ultimos 30 Dias</p>
+            <h3 class="title">R$34,245</h3>
           </template>
         </stats-card>
       </div>
@@ -148,15 +124,8 @@
           </template>
 
           <template slot="content">
-            <p class="category">Folowers</p>
-            <h3 class="title">+245</h3>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>update</md-icon>
-              Just Updated
-            </div>
+            <p class="category">Gastos Próximos 30 dias</p>
+            <h3 class="title">R$34,245</h3>
           </template>
         </stats-card>
       </div>
@@ -165,8 +134,8 @@
       >
         <md-card>
           <md-card-header data-background-color="orange">
-            <h4 class="title">Employees Stats</h4>
-            <p class="category">New employees on 15th September, 2016</p>
+            <h4 class="title">Ultimos gastos cadastrados</h4>
+            <!-- <p class="category">New employees on 15th September, 2016</p> -->
           </md-card-header>
           <md-card-content>
             <ordered-table table-header-color="orange"></ordered-table>
@@ -176,24 +145,15 @@
       <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
       >
-        <nav-tabs-card>
-          <template slot="content">
-            <span class="md-nav-tabs-title">Tasks:</span>
-            <md-tabs class="md-success" md-alignment="left">
-              <md-tab id="tab-home" md-label="Bugs" md-icon="bug_report">
-                <nav-tabs-table></nav-tabs-table>
-              </md-tab>
-
-              <md-tab id="tab-pages" md-label="Website" md-icon="code">
-                <nav-tabs-table></nav-tabs-table>
-              </md-tab>
-
-              <md-tab id="tab-posts" md-label="server" md-icon="cloud">
-                <nav-tabs-table></nav-tabs-table>
-              </md-tab>
-            </md-tabs>
-          </template>
-        </nav-tabs-card>
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Ultimos atualizações das regras fiscais</h4>
+            <!-- <p class="category">New employees on 15th September, 2016</p> -->
+          </md-card-header>
+          <md-card-content>
+            <ordered-table table-header-color="green"></ordered-table>
+          </md-card-content>
+        </md-card>
       </div>
     </div>
   </div>
@@ -202,18 +162,18 @@
 <script>
 import {
   StatsCard,
-  ChartCard,
-  NavTabsCard,
-  NavTabsTable,
+  // ChartCard,``
+  // NavTabsCard,
+  // NavTabsTable,
   OrderedTable,
 } from "@/components";
 
 export default {
   components: {
     StatsCard,
-    ChartCard,
-    NavTabsCard,
-    NavTabsTable,
+    // ChartCard,
+    // NavTabsCard,
+    // NavTabsTable,
     OrderedTable,
   },
   data() {
